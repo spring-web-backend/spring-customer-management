f0ebbadb42becdd53facdaa42e3fec9cc2014f9c
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-21 22:43:30 -0700

    mapping to edit form

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -7,6 +7,7 @@ import com.codegym.service.CustomerServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -33,4 +34,11 @@ public class CustomerController {
         redirect.addFlashAttribute("success", "Saved customer successfully!");
         return "redirect:/";
     }
+
+    @GetMapping("/customer/{id}/edit")
+    public String edit(@PathVariable int id, Model model) {
+        model.addAttribute("customer", customerService.findById(id));
+        return "edit";
+    }
+
 }
